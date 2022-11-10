4bf1c751d89990e9ac4f6126fe07e607b6edb0e2
Shenghao <shenghaodong18@gmail.com>
2022-11-10 09:22:56 -0500

    task a works

--- funcs.cpp
+++ funcs.cpp
@@ -1,36 +1,34 @@
 #include <iostream>
 #include "funcs.h"
 #include <cmath>
+#include "coord3d.h"
 
 // add functions here
 
 //Task A:
-double length(Coord3D *p){
-    int sum;
-    for(int i = 0; i < &p.length(); i++){
-        sum += &p * &p;
-    }
+double calcLength(Coord3D *p){
+    double sum = (p -> x * p -> x) + (p -> y * p -> y) + (p -> z * p -> z);
     sum = sqrt(sum);
     return sum;
 }
 
 //Task B:
-Coord3D * fartherFromOrigin(Coord3D *p1, Coord3D *p2){
+// Coord3D * fartherFromOrigin(Coord3D *p1, Coord3D *p2){
 
-}
+// }
 
 //Task C:
-void move(Coord3D *ppos, Coord3D *pvel, double dt){
+// void move(Coord3D *ppos, Coord3D *pvel, double dt){
 
-}
+// }
 
 //Task E:
-// allocate memory and initialize
-Coord3D* createCoord3D(double x, double y, double z){
+//allocate memory and initialize
+// Coord3D* createCoord3D(double x, double y, double z){
 
-}
+// }
 
-// free memory
-void deleteCoord3D(Coord3D *p){
+//free memory
+// void deleteCoord3D(Coord3D *p){
     
-}
+// }
--- main.cpp
+++ main.cpp
@@ -2,8 +2,11 @@
 
 
 #include "funcs.h"
+#include "coord3d.h"
 
 int main()
 {
+  Coord3D pointP = {10, 20, 30};
+  std::cout << calcLength(&pointP) << std::endl;
   return 0;
 }
