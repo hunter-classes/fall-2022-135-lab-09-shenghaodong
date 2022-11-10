14784b4f120a689585f8ff2e163ec2983de5d815
Shenghao Dong <shenghaodong18@gmail.com>
2022-11-09 23:15:49 -0500

    added placeholders for all funcs and poem.cpp (part d)

--- funcs.cpp
+++ funcs.cpp
@@ -3,6 +3,8 @@
 #include <cmath>
 
 // add functions here
+
+//Task A:
 double length(Coord3D *p){
     int sum;
     for(int i = 0; i < &p.length(); i++){
@@ -10,4 +12,25 @@ double length(Coord3D *p){
     }
     sum = sqrt(sum);
     return sum;
+}
+
+//Task B:
+Coord3D * fartherFromOrigin(Coord3D *p1, Coord3D *p2){
+
+}
+
+//Task C:
+void move(Coord3D *ppos, Coord3D *pvel, double dt){
+
+}
+
+//Task E:
+// allocate memory and initialize
+Coord3D* createCoord3D(double x, double y, double z){
+
+}
+
+// free memory
+void deleteCoord3D(Coord3D *p){
+    
 }
