e72605c21e9bfd105ae0857094cf0984aad27545
Shenghao Dong <shenghaodong18@gmail.com>
2022-11-09 23:05:02 -0500

    added part a, have to test

--- funcs.cpp
+++ funcs.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 #include "funcs.h"
+#include <cmath>
 
 // add functions here
-
+double length(Coord3D *p){
+    int sum;
+    for(int i = 0; i < &p.length(); i++){
+        sum += &p * &p;
+    }
+    sum = sqrt(sum);
+    return sum;
+}
