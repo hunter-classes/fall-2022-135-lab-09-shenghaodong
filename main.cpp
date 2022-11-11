c5a4865db5d418d79050bf8a912c9dd9e80a8837
Shenghao Dong <shenghaodong18@gmail.com>
2022-11-10 23:16:10 -0500

    made main output look cleaner

--- main.cpp
+++ main.cpp
@@ -12,18 +12,27 @@ int main()
   Coord3D vel = {1, -5, 0.2};
 
   //Part A
+  std::cout << "Part A: " << std::endl;
   std::cout << calcLength(&pointP) << std::endl;
+  std::cout << "\n";
 
   //Part B
+  std::cout << "Part B: " << std::endl;
   std::cout << fartherFromOrigin(&pointP, &pointQ) << std::endl;
+  std::cout << "\n";
 
   //Part C
   move(&pos, &vel, 2.0); // object pos gets changed
+  std::cout << "Part C: " << std::endl;
   std::cout << pos.x << " " << pos.y << " " << pos.z << std::endl;
+  std::cout << "\n";
 
   //Part E
+  std::cout << "Part E: " << std::endl;
   Coord3D *randomVal = createCoord3D(4.1, 5.2, 3.2);
   std::cout << (*randomVal).x << " " << (*randomVal).y << " " << (*randomVal).z << std::endl;
   deleteCoord3D(randomVal);
+  std::cout << "\n";
+  
   return 0;
 }
